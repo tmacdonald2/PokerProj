5797da5c654de306dc0e9332ee27323102283bd1
Jesse Bond <jesse.o.bond@gmail.com>
2020-04-07 13:46:30 -0700

    updating game.java file

---
+++ game.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+public class Game{
+    private boolean winner;
+    private player first;
+    private player second;
+    private player third;
+    private LinkedList<card> deck;
+    private LinkedList<card> community;
+    private int mainPot;
+    private int secPot;
+    private int dealerButton;
+
+    public Game(){
+        //runs the game until winner == true
+    }
+
+    private static void shuffleDeck(){
+        //function for shuffling deck
+    }
+
+    private static void preflop(){
+        //function for preflop action
+    }
+
+    private static void flop(){
+        //function for flop betting
+    }
+
+    private static void turn(){
+        //function for turn betting
+    }
+
+    private static void river(){
+        //function for river betting
+    }
+
+}
