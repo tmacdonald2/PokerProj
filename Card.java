37858f35bfbbbc79d048110dd170d904243326fc
Trevor MacDonald <60413413+tmacdonald2@users.noreply.github.com>
2020-04-22 15:43:48 -0700

    Updated Deck and Card classes

--- Card.java
+++ Card.java
@@ -1,24 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Card {
 
-    private String rank;      //2-10,J,Q,K,A, this is string so we can output Jack, Queen, King, and Ace
-    private String suit;      //clubs, diamonds, hearts, spades
+    private String rank;
+    private String suit;
 
-    int initializer = 0;     // used to get the next card in the text file
+    static int initializer = 0;     // Used to get the next card in the text file
     Card tempCard;
+    String[] tempArray; //possible error
 
+
+    // Generates a card. Gets card data getCardData(), splits it into rank and suit, then creates the card.
     public Card()
     {
-        // take in the first card from the text file of 52 cards, uses initializer
+        String tempCardString = getCardData();
 
-        String tempRank = "Ace";       // Ace to be replaced with value from text file
-        String tempSuit = "Spades";     // Spades to be replaced with value from text file
+        tempArray = tempCardString.split(",");
 
-        initializer++;
+        String tempRank = tempArray[0];
+        String tempSuit = tempArray[1];
 
         tempCard = new Card(tempRank, tempSuit);
     }
 
 
+    // Gets a card's data from the text file and sends it to Card().
+    public String getCardData()
+    {
+        try
+        {
+            BufferedReader reader = null;
+            reader = new BufferedReader(new FileReader("cardDatabase.txt"));
+            String cardLineData = null;
+
+            initializer+=1;
+            for(int j = 0; j < initializer; j++)
+                cardLineData = reader.readLine();
+
+            return cardLineData; //a string
+        }
+        catch(IOException e)
+        {
+            return("error");
+        }
+    }
+
+
+    // Used by Card() to set this cards rank and suit. Needed for getCard() to work.
     public Card(String rank, String suit)
     {
         this.rank = rank;
@@ -26,10 +56,16 @@ public class Card {
     }
 
 
+    // Returns a card with a rank and suit.
+    public Card getCard() {
+        return tempCard;
+    }
+
+
+    // Used when resetting the deck.
     public void resetInitializer()
     {
         initializer = 0;
-        // Used when resetting the deck.
     }
 
 
@@ -37,16 +73,20 @@ public class Card {
         return rank;
     }
 
+
     public String getSuit() {
         return suit;
     }
 
 
+                                            /*--------------- Anchors ----------------*/
 
 
-
-    public Card getCard() {
-        return tempCard;
+    // Displays the rank and suit of the card.
+    // I used this when I drew a card and wanted to make sure it was the first card in the deck.
+    public String toString()
+    {
+        return (rank + " of " + suit);
     }
 }
 
--- Deck.java
+++ Deck.java
@@ -1,54 +1,69 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 
-    ArrayList<Card> cardDeck;   // The deck - an ArrayList of 52 cards.
+    ArrayList<Card> cardDeck = new ArrayList<Card>();   // The deck - an ArrayList of 52 cards.
     Card tempCard, firstCard;
 
 
+    // When the constructor is called, calls setDeck() which generates then sorts a deck.
     public Deck()
     {
         setDeck();
     }
 
 
+    // Generates a sorted deck, then shuffles.
     public void setDeck()
     {
-        // Store all 52 cards into the cardDeck array list.
-        cardDeck = new ArrayList<Card>();
-
         for (int i = 0; i < 52; i++)
         {
-            tempCard = new Card();
-
-            cardDeck.add(tempCard.getCard());
+            tempCard = new Card();  // Calls card constructor
+            cardDeck.add(tempCard.getCard());   // Get card just returns the card created from the constructor
         }
 
         shuffleDeck();
     }
 
 
+    // Shuffles the deck.
     public void shuffleDeck()
     {
-        // Shuffles the deck
-        // Ensures the deck has 52 cards before shuffling
+        Collections.shuffle(cardDeck);
     }
 
 
-    public void resetDeck()
-    {
-        tempCard.resetInitializer();
-        setDeck();
-    }
+                                        /*--------------- Anchors ----------------*/
 
 
-    // Returns the first card in the deck - anchor
-    public Card getFirstCard()
+    // Returns the first card in the deck. If the deck is empty, reset the deck.
+    public Card drawCard()
     {
+        if(cardDeck.size() == 0)
+            resetDeck();
+
         firstCard = cardDeck.get(0);
 
         cardDeck.remove(0);
 
         return firstCard;
+    }
+
+
+    // Resets the deck.
+    public void resetDeck()
+    {
+        cardDeck = new ArrayList<Card>();
+        tempCard.resetInitializer();
+        setDeck();
+    }
+
+
+    // Can be used to display the current cardDeck arrayList. Really only used for testing.
+    public void displayDeck()
+    {
+        for (int i = 0; i < cardDeck.size();i++)
+            System.out.println(cardDeck.get(i).getRank() + " of " + cardDeck.get(i).getSuit());
     }
 }
